c72e0e238ce24d97c193495d10d5181a8cecc303
rjsk <rjsk@zenwalk-darkstar-dynamulti9>
2022-06-28 11:16:21 +0900

    done working with variables

---
+++ src/secondsInYearsEx.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int seconds_per_year = 365*24*60*60;
+
+  std::cout << seconds_per_year << std::endl;
+  std::cout << seconds_per_year *2 << std::endl;
+  std::cout << seconds_per_year *5 << std::endl;
+  std::cout << seconds_per_year *10 << std::endl;
+}
